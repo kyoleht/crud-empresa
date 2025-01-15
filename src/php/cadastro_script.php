64d0dcd77a73dc547ef5ae88a043c240c030b590
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-01-14 21:59:37 -0300

    /

--- src/php/cadastro_script.php
+++ src/php/cadastro_script.php
@@ -24,9 +24,13 @@
 
                 $hash_senha = password_hash($senha, PASSWORD_BCRYPT);
 
-                $sql = "INSERT INTO pessoas (nome, endereco, telefone, dt_nascimento, email, senha) VALUES ('$nome', '$endereco', '$telefone', '$dt_nascimento', '$email', '$senha')";
+                $foto = $_FILES['foto'];
+                $nome_foto = mover_foto($foto);
+
+                $sql = "INSERT INTO pessoas (nome, endereco, telefone, dt_nascimento, email, senha, foto) VALUES ('$nome', '$endereco', '$telefone', '$dt_nascimento', '$email', '$senha', '$nome_foto')";
 
                 if (mysqli_query($conn, $sql)) {
+                    echo "<img src='img/$nome_foto' title='$nome_foto' class='mostra_foto'>";
                     mensagem("$nome foi cadastrado com sucesso", 'success');
                 } else {
                     mensagem("$nome NÃO foi cadastrado!", 'danger');
